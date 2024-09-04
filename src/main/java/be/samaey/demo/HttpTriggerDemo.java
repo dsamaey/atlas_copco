8bace6bb2bff5a32030248142a4b2c8721304422
Diether Samaey <diether.samaey@gmail.com>
2024-09-04 14:45:46 +0200

    Renamed function class

---
+++ src/main/java/be/samaey/demo/HttpTriggerDemo.java
@@ -0,0 +1,17 @@
+package be.samaey.demo;
+
+import java.util.*;
+import com.microsoft.azure.functions.annotation.*;
+import com.microsoft.azure.functions.*;
+
+public class HttpTriggerDemo {
+
+    @FunctionName("demo")
+    public HttpResponseMessage run(
+            @HttpTrigger(name = "req", methods = {HttpMethod.GET, HttpMethod.POST}, authLevel = AuthorizationLevel.ANONYMOUS) HttpRequestMessage<Optional<String>> request,
+            final ExecutionContext context) {
+        context.getLogger().info("Java HTTP trigger processed a request.");
+
+        return request.createResponseBuilder(HttpStatus.OK).body("Seems to work").build();
+    }
+}
